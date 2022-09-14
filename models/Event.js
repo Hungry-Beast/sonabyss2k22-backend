da89840582123a98bcc42f294b58c954d566c4e8
Hibjul Ahmed <hibjulahmed@gmail.com>
2022-09-14 16:41:10 +0530

    Events added

--- models/Event.js
+++ models/Event.js
@@ -12,9 +12,17 @@ const EventSchema = new Schema(
       type: String,
       required,
     },
+    time: {
+      type: String,
+      required: true,
+    },
     club: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Club",
+      required,
+    },
+    clubName: {
+      type: String,
       required,
     },
     image: {
---
+++ routes/Event.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} = require("firebase/storage");
+const storage = getStorage();
+
+const Event = require("../models/Event");
+
+router.post("/", multer().single("file"), async (req, res) => {
+  try {
+    let metadata = {
+      contentType: req.file.mimetype,
+      name: req.file.originalname,
+    };
+    // storage.put(req.file.buffer, metadata);
+    // }
+    const storageRef = ref(storage, `${req.file.originalname}`);
+    const snapshot = await uploadBytes(storageRef, req.file.buffer, metadata);
+    const downloadUrl = await getDownloadURL(snapshot.ref);
+
+    const EventData = await Event.create({
+      name: req.body.name,
+      date: req.body.date,
+      time: req.body.time,
+      club: req.body.clubId,
+      clubName: req.body.clubName,
+      image: downloadUrl,
+      desc: req.body.desc,
+    });
+    res.json(EventData);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error!");
+  }
+});
+
+module.exports = router;
