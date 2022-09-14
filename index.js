4375fd7470416ffc8adf91fb8cfb5c83992d61ad
Hibjul Ahmed <hibjulahmed@gmail.com>
2022-09-14 12:32:25 +0530

    Club route added

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('./firebaseAuth')
+require("./firebaseAuth");
 const mongoConnect = require("./db");
 const Club = require("./routes/Club");
 mongoConnect();
 app.use(cors());
+app.use("/clubs", Club);
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/", (req, res) => {
+  res.send("I am listening");
+});
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
