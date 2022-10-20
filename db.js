cf16c74fa049443ef1985350ff10fe993c789b2d
Subash Paul <subashpaul559@gmail.com>
2022-10-21 02:17:59 +0530

    Fixed organisers

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv/config");
 mongoose_URI = process.env.DB_KEY;
 mongoConnect = async () => {
-//   console.log("hi");
+  //   console.log("hi");
   try {
     await mongoose.connect(mongoose_URI, () => {
       console.log("Connected to mongo Successfully!");
--- routes/Organisers.js
+++ routes/Organisers.js
@@ -17,7 +17,7 @@ const { async } = require("@firebase/util");
 const User = require("../models/User");
 const Organisers = require("../models/Organisers");
 
-router.post("/", [fetchAdmin, multer().single("file")], async (req, res) => {
+router.post("/", multer().single("file"), async (req, res) => {
   try {
     if (!req.file) {
       res.status(206).send("Please insert a image");
@@ -37,8 +37,13 @@ router.post("/", [fetchAdmin, multer().single("file")], async (req, res) => {
       name: req.body.name,
       regNo: req.body.regNo,
       image: downloadUrl,
-      group: req.body.group,    
+      group: req.body.group,
       position: req.body.position,
+      linkedin: req.body.linkedin,
+      insta: req.body.insta,
+      fb: req.body.fb,
+      github: req.body.github
+
     });
     res.json(organiser);
   } catch (error) {
@@ -47,12 +52,19 @@ router.post("/", [fetchAdmin, multer().single("file")], async (req, res) => {
   }
 });
 router.get("/", async (req, res) => {
-  const organiser1 = await Organisers.find({ group: 1 });
-  const organiser2 = await Organisers.find({ group: 1 });
+  const Developers = await Organisers.find({ group: 1 });
+  const Creative_Head = await Organisers.find({ group: 2 });
+  const PubAndBrand = await Organisers.find({ group: 3 });
+  const StageIncharge = await Organisers.find({ group: 4 });
+  const SonabyssIncharge = await Organisers.find({ group: 5 });
+  const Hospitality = await Organisers.find({ group: 6 });
+  const Media = await Organisers.find({ group: 7 });
+  const Org_secy = await Organisers.find({ group: 8 });
+  const Auditor = await Organisers.find({ group: 9 });
+  const Cosplay = await Organisers.find({ group: 10 });
 
   res.json([
-    { group: 1, organisers1 },
-    { group: 2, organiser2 },
+    Developers, Creative_Head, PubAndBrand, StageIncharge, SonabyssIncharge, Hospitality, Media, Org_secy, Auditor, Cosplay
   ]);
 });
 
